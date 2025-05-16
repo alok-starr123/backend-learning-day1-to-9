dac27bc32f1aa96c5079e5cacf8db0d281a0fd87
alok-starr123 <as4692341@gmail.com>
2025-05-16 22:41:59 +0530

    DAY 18: Refactored Node.js MongoDB API using MVC structure

---
+++ day18-api-refactor/server.js
@@ -0,0 +1,21 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch((err) => console.error("Connection failed:", err));
+
